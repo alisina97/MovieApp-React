997014887e9cb309e6bbda293b7210053a2996fc
Ali Sina <ali.sina1447@gmail.com>
2024-10-18 13:49:11 -0400

    ADD - axiosConfig.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './api/axiosConfig'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="Logo representing React" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload to see your changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="Learn more about React"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
