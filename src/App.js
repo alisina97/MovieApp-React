50e1dd5f501def73aa8f3c2883094c6c9d8329c4
Ali Sina <ali.sina1447@gmail.com>
2024-10-18 19:43:41 -0400

    UPDATE - set up browser router and added Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import api from './api/axiosConfig'
+import api from './api/axiosConfig';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -18,16 +21,19 @@ function App() {
     }
   }
   
-
   useEffect(() => {
     getMovies();
   }, [])
 
   return (
-    <div className="App">
+    <div className='App'>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Home/>} />
+      </Route>
+    </Routes>
     </div>
   );
 }
 
 export default App;
-
