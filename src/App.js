47af9b0488ec4f58b3b3f617c7b6a4d2ca8b8f4f
Ali Sina <ali.sina1447@gmail.com>
2024-10-18 15:06:48 -0400

    UPDATE - set up GET HTTP request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import api from './api/axiosConfig'
 
 function App() {
+
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () =>{
+    try {
+
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
     <div className="App">
-      
     </div>
   );
 }
