c153de3e2a38daee0f29741ff0f3d9635bfbc85f
Ali Sina <ali.sina1447@gmail.com>
2024-10-18 20:56:05 -0400

    ADD - Hero component to home page

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     <div className='App'>
     <Routes>
       <Route path='/' element={<Layout/>}>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home movies={movies} />} />
       </Route>
     </Routes>
     </div>
