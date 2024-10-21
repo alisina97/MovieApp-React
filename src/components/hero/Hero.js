c429ecbe1d00e4eec92ce59244da426bc6e47abe
Ali Sina <ali.sina1447@gmail.com>
2024-10-21 15:03:05 -0400

    ADD - trailer play icon

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import { Paper } from '@mui/material'
 import './Hero.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons'
 
 function Hero({movies}) {
   return (
@@ -19,6 +21,13 @@ function Hero({movies}) {
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{movie.title}</h4>
+                                        </div>
+                                        <div className='movie-button-container'>
+                                            <div className='play-button-icon-container'>
+                                                <FontAwesomeIcon className='play-button-icon'
+                                                    icon={faCirclePlay}
+                                                />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
