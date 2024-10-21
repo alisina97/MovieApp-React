8c0784f7ebe53b3fe3b0eb817bccbc72ac116407
Ali Sina <ali.sina1447@gmail.com>
2024-10-21 12:47:32 -0400

    FIX - added initial state to movies state hook to display the data

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Home from './components/home/Home';
 
 function App() {
 
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () =>{
     try {
-
       const response = await api.get("/api/v1/movies");
       console.log(response.data);
       setMovies(response.data);
@@ -28,7 +27,7 @@ function App() {
   return (
     <div className='App'>
     <Routes>
-      <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Layout/>}>
         <Route path='/' element={<Home movies={movies} />} />
       </Route>
     </Routes>
