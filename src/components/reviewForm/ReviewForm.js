317b72bc5b92821cf9cbfdd5440ed9e2514d1378
Ali Sina <ali.sina1447@gmail.com>
2024-10-26 14:22:53 -0400

    ADD & UPDATE - updated review form component and added review component

--- src/components/reviewForm/ReviewForm.js
+++ src/components/reviewForm/ReviewForm.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {Form, Button, FormGroup, FormLabel, FormControl} from 'react-bootstrap'
 
-function reviewForm() {
+function reviewForm({handleSubmit, revText, labelText, defaultValue}) {
   return (
-    <div>reviewForm</div>
+    <Form>
+        <FormGroup className='b-3' controlId='exampleForm.controlTextarea1'>
+            <FormLabel> {labelText}</FormLabel>
+            <FormControl ref={revText} as="textarea" rows={3} defaultValue={defaultValue} />
+        </FormGroup>
+        <Button variant='outline-info' onClick={handleSubmit}>Submit</Button>
+    </Form>
   )
 }
 
